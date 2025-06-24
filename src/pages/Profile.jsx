4dd4e2487dec1414965def401b6ed36f96d3ce59
Dmitry <103138302+dimi-try@users.noreply.github.com>
2025-06-24 14:59:07 +0400

    prepare update profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,12 +4,10 @@ import api from "../hooks/api";
 const Profile = () => {
   // Получаем ID энергетика из URL
   const id = 1;
-  // Состояние для данных об энергетике
-  const [user, setUser] = useState(null);
+  // Состояние для данных пользователя
+  const [profile, setProfile] = useState(null);
   // Состояние для списка отзывов
   const [reviews, setReviews] = useState([]);
-  // Состояние для критериев оценки
-  const [criteria, setCriteria] = useState([]);
   // Состояние для ошибок
   const [error, setError] = useState(null);
 
@@ -19,15 +17,13 @@ const Profile = () => {
     
     const fetchData = async () => {
       try {
-        const [userRes, reviewsRes, criteriaRes] = await Promise.all([
+        const [profileRes, reviewsRes] = await Promise.all([
           api.get(`/users/${id}/profile`),
-          api.get(`/users/${id}`),///reviews/
-          api.get(`/criteria/`)
+          api.get(`/users/${id}/reviews`)
         ]);
   
-        setUser(userRes.data); // Сохраняем данные об энергетике
-        setReviews(reviewsRes.data); // Сохраняем отзывы
-        setCriteria(criteriaRes.data); // Сохраняем критерии
+        setProfile(profileRes.data); // Сохраняем данные о пользователе
+        setReviews(reviewsRes.data); // Сохраняем все отзывы пользователя
       }
       catch (err) {
         setError(err.message); // Сохраняем ошибку
@@ -36,37 +32,10 @@ const Profile = () => {
     fetchData();
   }, [id]);
   
-  // const user = {
-  //   name: "Иван",
-  //   reviews: 12,
-  //   avgRating: 7.8,
-  //   favoriteEnergy: "Monster Ultra",
-  //   favoriteBrand: "Monster",
-  //   reviewsList: [
-  //     { brand: "Red Bull", energy: "Red Bull Original", scores: [8, 9, 7], text: "Хороший энергетик!" },
-  //   ],
-  // };
-
   return (
     <div>
-      {reviews.email}
+      
     </div>
-    // <div>
-    //   <h1>{user.username}</h1>
-    //   <p>Отзывы: {user.reviews}</p>
-    //   <p>Средняя оценка: {user.avgRating}</p>
-    //   <p>Любимый энергетик: {user.favoriteEnergy}</p>
-    //   <p>Любимый бренд: {user.favoriteBrand}</p>
-
-    //   <h2>Отзывы</h2>
-    //   {user.reviewsList.map((r, i) => (
-    //     <div key={i}>
-    //       <h3>{r.brand} - {r.energy}</h3>
-    //       {r.scores.map((s, j) => <p key={j}>Критерий {j+1}: {s}</p>)}
-    //       <p>{r.text}</p>
-    //     </div>
-    //   ))}
-    // </div>
   );
 };
 
