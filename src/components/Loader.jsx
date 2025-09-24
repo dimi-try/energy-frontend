cb532db935e2129f8c1447ee56cc3b069d285888
Dima M <103138302+dimi-try@users.noreply.github.com>
2025-09-24 13:15:34 +0400

    minor fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Logo from "../styles/logo512.png";
+import Logo from "../images/LoadIcon.png";
 import Card from "./Card";
 import "./Loader.css";
 
